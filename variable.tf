2dbdb39470cf82c638878383a5ea4fe8222761c1
biradarajay1904 <99815064+biradarajay1904@users.noreply.github.com>
2025-11-14 17:00:05 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "region"         { default = "ap-south-1" }
+variable "project"        { default = "practice" }
+variable "env"            { default = "dev" }
+variable "vpc_cidr"       { default = "10.0.0.0/16" }
+variable "public_subnets" { default = ["10.0.1.0/24", "10.0.2.0/24"] }
